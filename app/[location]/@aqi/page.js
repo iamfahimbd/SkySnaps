424d81f089752ad4d964a20ee804c11dc184e35f
Fahim <iamfahim.bd@gmail.com>
2025-08-01 23:21:50 +0600

    vercel issue fixing

--- app/[location]/@aqi/page.js
+++ app/[location]/@aqi/page.js
@@ -3,16 +3,16 @@ import NoLocationInfo from "@/components/NoLocationInfo";
 import { getResolvedLatLong } from "@/lib/location-info";
 
 const AQIPage = async ({
-    params: { location },
-    searchParams: { latitude, longitude },
+  params: { location },
+  searchParams: { latitude, longitude },
 }) => {
-    const resolved = await getResolvedLatLong(location, latitude, longitude);
+  const resolved = await getResolvedLatLong(location, latitude, longitude);
 
-    if (resolved?.lat && resolved?.lon) {
-        return <AQIComponent lat={resolved.lat} lon={resolved.lon} />;
-    } else {
-        return <NoLocationInfo />;
-    }
+  if (resolved?.lat && resolved?.lon) {
+    return <AQIComponent lat={resolved.lat} lon={resolved.lon} />;
+  } else {
+    return <NoLocationInfo />;
+  }
 };
 
 export default AQIPage;
