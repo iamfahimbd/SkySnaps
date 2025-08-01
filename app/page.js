7e5a2610b38ba746531749b68a88e60e1070a82b
Fahim <iamfahim.bd@gmail.com>
2025-08-01 22:08:38 +0600

    file name and app/page.js file changed for deployement issues

--- app/page.js
+++ app/page.js
@@ -1,11 +1,14 @@
 import LocationDetector from "@/components/LocationDetector";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
     <div>
       <p className="text-center text-3xl"> Alhamdulillah...</p>
 
-      <LocationDetector />
+      <Suspense fallback={<div>Loading...</div>}>
+        <LocationDetector />
+      </Suspense>
     </div>
   );
 }
